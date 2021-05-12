ef67a6a6a4542e2b6a01a2b1700614f5ce752059
yuri nakanishi <72235392+yuri-tyrian@users.noreply.github.com>
2021-05-12 14:32:38 +0900

    Create resze.py

---
+++ resze.py
@@ -0,0 +1,7 @@
+import PIL
+from PIL import Image
+PIL.Image.MAX_IMAGE_PIXELS =None
+img = Image.open('IMAGE YOU WANT TO RESIZE')
+
+img_resize = img.resize((img.width // 4, img.height // 4)) #This time reduced to 1/4
+img_resize.save('NEW IMAGE NAME)
