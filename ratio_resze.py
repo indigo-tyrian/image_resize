4e83884f6b0fb41e072af9203585516308a811ec
yuri nakanishi <72235392+yuri-tyrian@users.noreply.github.com>
2021-05-12 14:36:56 +0900

    Update and rename resze.py to  ratio_resze.py

--- ratio_resze.py
+++ ratio_resze.py
@@ -4,4 +4,4 @@ PIL.Image.MAX_IMAGE_PIXELS =None
 img = Image.open('IMAGE YOU WANT TO RESIZE')
 
 img_resize = img.resize((img.width // 4, img.height // 4)) #This time reduced to 1/4
-img_resize.save('NEW IMAGE NAME)
+img_resize.save('NEW IMAGE NAME')
